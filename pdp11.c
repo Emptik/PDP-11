a0abc6757ef403b7e58b1872cbd6f601c0d70ccd
Emptik <37045855+Emptik@users.noreply.github.com>
2018-03-25 23:36:53 +0300

    Add files via upload

---
+++ pdp11.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#define _CHECK(x, y) if( x == EOF)\
+{\
+free(val);\
+free(a);\
+free(num);\
+assert(y);\
+break;\
+}\
+
+typedef unsigned char byte;
+typedef unsigned short int word;
+typedef short adr;
+
+byte mem[64 * 1024];
+
+byte b_read  (adr a);					//читает из "старой памяти" mem байт с "адресом" a.
+void b_write (adr a, byte val);	// пишет значение val в "старую память" mem в байт с "адресом" a.
+word w_read  (adr a);					// читает из "старой памяти" mem слово с "адресом" a.
+void w_write (adr a, word val);	// пишет значение val в "старую память" mem в слово с "адресом" a.
+void load_file();
+
+void mem_dump(adr start, word n);
+void test_mem();
+
+int main() {
+	test_mem();
+	return 0;
+}
+
+word w_read(adr a) {
+	assert(!(a % 2));
+	word val = 0xffff;
+	val = val & (((word)mem[a + 1]) << 8);
+	val = val + (word)mem[a];
+	return val;
+}
+
+void w_write(adr a, word val) {
+	assert(!(a % 2));
+	mem[a + 1] = (byte)(val >> 8);
+	mem[a] = (byte)((val << 8) >> 8);
+}
+
+byte b_read(adr a){
+	return mem[a];
+}
+
+void b_write(adr a, byte val){
+	mem[a] = val;
+}
+
+void load_file()
+{
+	adr counter = 0;
+	adr check = 0;
+	unsigned int * val = calloc(1, sizeof(unsigned int));
+	unsigned int * num = calloc(1, sizeof(unsigned int));
+	unsigned int * a = calloc(1, sizeof(unsigned int));
+	FILE * f_in = stdin;
+	assert(f_in);
+	for( ; ; )
+	{
+		check = fscanf(f_in, "%04x", a);
+		_CHECK(check, 1);
+		check = fscanf(f_in, "%04x", num);
+		_CHECK(check, 0);
+		for(counter = 0; counter < (*num); counter++)
+		{
+			check = fscanf(f_in, "%02x", val);
+			_CHECK(check, 0);
+			b_write((*a) + counter, *val);
+		}
+	}
+}
+
+void mem_dump(adr start, word n) {
+	int i = 0;
+	for(; i < n; i += 2)
+	{
+		printf("%06o : ", start + i);
+		printf("%06o\n", w_read(start + i));
+	}
+}
+
+void test_mem()
+{
+	load_file();
+	mem_dump(0x40, 4);
+}
