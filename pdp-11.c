3c5a50475c0848c73d4d7e3252fb51a43ec9f106
vlad <Korovin.Va@phystech.edu>
2018-04-16 20:55:59 +0300

    try_1

--- pdp-11.c
+++ pdp-11.c
@@ -8,13 +8,22 @@
 assert((y));\
 fclose((f_in));\
 break;\
-}\
+}
+#define NO_PARAM 0
+#define HAS_SS 1
+#define HAS_DD (1<<1)
+#define HAS_XX (1<<2)
+#define HAS_NN (1<<3)
 
 typedef unsigned char byte;
 typedef unsigned short int word;
 typedef short adr;
 
 byte mem[64 * 1024];
+word reg[8];
+
+word nn;
+word ss;
 
 byte b_read  (adr a);					//читает из "старой памяти" mem байт с "адресом" a.
 void b_write (adr a, byte val);	// пишет значение val в "старую память" mem в байт с "адресом" a.
@@ -24,6 +33,9 @@ void test_mem();
 
 void load_file(char * file_name);
 void mem_dump(adr start, word n);
+
+word get_nn();
+word get_ss();
 
 void run(adr pc);
 void do_halt();
@@ -36,17 +48,18 @@ struct Command {
 	word mask;
 	char * name;
 	void (*func)();
+	byte param;
 } command[] = {
-	{0, 0xFFFF, "halt", do_halt},
-	{0010000, 0170000, "mov", do_mov},
-	{0060000, 0170000, "add", do_add},
+	{0, 0xFFFF, "halt", do_halt, NO_PARAM},
+	{0010000, 0170000, "mov", do_mov, HAS_SS | HAS_DD},
+	{0060000, 0170000, "add", do_add, HAS_SS | HAS_DD},
 	{0000000, 0000000, "unknown", do_unknown} // Must be the last
 };
 
 int main(int argc, char **argv) {
 	test_mem();
 	load_file(argv[1]);
-	run(0x40);
+	run(0x1000);
 	return 0;
 }
 
@@ -117,11 +130,20 @@ void run(adr pc0)
 	{
 		word w = w_read(pc);
 		printf("%06o:%06o\n", pc, w);
-		for(i = 0; i <= 3;i++)
+		for(i = 0; i <= 3; i++)
 		{
 			struct Command cmd = command[i];
 			if((w & cmd.mask) == cmd.opcode)
 			{
+				if(cmd.param & HAS_NN)
+				{
+					nn = get_nn(w);
+				}
+				if(cmd.param & HAS_SS)
+				{
+					ss = get_ss(w);
+				}
+				printf("%s", cmd.name);
 				cmd.func();
 				break;
 			}
@@ -152,6 +174,11 @@ void do_unknown()
 }
 
 void test_mem()
+{
+	
+}
+
+word get_ss()
 {
 	
 }
